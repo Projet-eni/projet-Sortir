41912927b75f8ef3b63cd1f37469f6b6124dbc09
Shack-dev <gaetan.martin2019@campus-eni.fr>
2020-04-28 15:05:15 +0200

    Merge branch 'master' of C:\wamp64\www\Projet-Sortir with conflicts.

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -20,7 +20,6 @@ class SortieController extends AbstractController
         $sorties = $repository->findAll();
 
 
-
-        return $this->render('sortie/listeSortie.html.twig',['sorties'=>$sortie]);
+        return $this->render('sortie/listeSortie.html.twig',['sorties'=>$sorties]);
     }
 }
